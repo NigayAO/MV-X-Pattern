bf54bb9d4eaf4e68538bca1f1ce00f00848ea5f6
Alik <Nigay_AO@bk.ru>
2022-03-01 17:53:51 +0300

    Initial Commit

---
+++ MV(X) Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MV(X) Pattern
+//
+//  Created by Alik Nigay on 01.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
